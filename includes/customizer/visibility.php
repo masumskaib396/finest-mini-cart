b89294923ec739985a055f2e5a28c30d3a2a070d
Masum Sakib <masumsakib396@gmail.com>
2021-11-23 12:58:52 +0600

    adding some fields

--- includes/customizer/visibility.php
+++ includes/customizer/visibility.php
@@ -1,15 +1,158 @@
 <?php
-Kirki::add_section( 'finest_settings', array(
-    'title'          => esc_html__( 'General Options', 'finest-mini-cart' ),
-    'panel'          => 'finest_panel',
+Kirki::add_section( 'visibility_section', array(
+    'title'          => esc_html__( 'Visibility Section', 'finest-mini-cart' ),
+    'panel'          => 'fmc_panel',
     'priority'       => 160,
 ) );
 
-Kirki::add_field( 'finest_panel', [
+Kirki::add_field( 'fmc_panel', [
     'type'        => 'switch',
-    'settings'    => 'on_quick_view',
-	'label'       => esc_html__( 'Show Quick View', 'finest-mini-cart' ),
-    'section'     => 'finest_settings',
+    'settings'    => 'fmc_shopping_utton',
+	'label'       => esc_html__( 'Show Continue Shopping Button', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_shipping_total',
+	'label'       => esc_html__( 'Show Shipping Total', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_tax_total',
+	'label'       => esc_html__( 'Show Tax Total', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_product_ount',
+	'label'       => esc_html__( 'Show Product Count', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_qty_box',
+	'label'       => esc_html__( 'Display Qty Box', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_product_image',
+	'label'       => esc_html__( 'Show Product Image', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_product_image',
+	'label'       => esc_html__( 'Show Product Image', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_product_price',
+	'label'       => esc_html__( 'Product Price', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_show_delete',
+	'label'       => esc_html__( 'Show Delete', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_coupon_filed',
+	'label'       => esc_html__( 'Show Coupon Filed', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_close_button',
+	'label'       => esc_html__( 'Show Close Button', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
+    'default'     => 'on',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'finest-mini-cart' ),
+        'off' => esc_html__( 'Disable', 'finest-mini-cart' ),
+    ],
+] );
+
+Kirki::add_field( 'fmc_panel', [
+    'type'        => 'switch',
+    'settings'    => 'fmc_subtotal',
+	'label'       => esc_html__( 'Show Subtotal', 'finest-mini-cart' ),
+    'section'     => 'visibility_section',
     'default'     => 'on',
     'priority'    => 10,
     'choices'     => [
